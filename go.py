0fffe36e045a78ce5005af141f819a51de8a12d5
Dan <dansonlinepresence@gmail.com>
2017-12-26 18:25:12 -0500

    initial commit

---
+++ go.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import os
+import subprocess
+import sys
+
+sys.path.append(os.path.abspath('src'))
+
+subprocess.check_call('git submodule update --init --recursive'.split())
+
+try: os.mkdir('built')
+except: pass
+os.chdir('built')
+subprocess.check_call('cmake -DCMAKE_BUILD_TYPE=Release ../build'.split())
+subprocess.check_call('cmake --build . --config Release'.split())
+
+from dansmidieditor import main
+main()
---
+++ src/dansmidieditor.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import argparse
+import time
+
+from config import controls
+import media
+
+def main():
+	while not controls.done:
+		while True:
+			event=media.poll_event()
+			if not event: break
+			controls.input(event)
+		controls.view.draw(media)
+		time.sleep(0.01)
+
+if __name__=='__main__':
+	parser=argparse.ArgumentParser()
+	parser.add_argument('--command', '-c', action='append', default=[])
+	args=parser.parse_args()
+
+	for i in args.command: controls.command(i)
+
+	main()
