8c4af2b79ada44df3ec40b7ba4282cd8756ce569
Dan <dansonlinepresence@gmail.com>
2018-01-07 21:29:19 -0500

    move sfml wrapper into danssfml

--- go.py
+++ go.py
@@ -6,12 +6,12 @@ import sys
 
 sys.path.append(os.path.abspath('src'))
 
-subprocess.check_call('git submodule update --init --recursive'.split())
+#subprocess.check_call('git submodule update --init --recursive'.split())
 
 try: os.mkdir('built')
 except: pass
 os.chdir('built')
-subprocess.check_call('cmake -DCMAKE_BUILD_TYPE=Release ../build'.split())
+subprocess.check_call('cmake -DCMAKE_BUILD_TYPE=Release ../deps/danssfml/wrapper'.split())
 subprocess.check_call('cmake --build . --config Release'.split())
 
 from dansmidieditor import main
--- src/media.py
+++ src/media.py
@@ -1,9 +1,17 @@
-import ctypes, os
+import ctypes
+import glob
+import os
+import pprint
+
 home=os.path.dirname(os.path.realpath(__file__))
-for path in ['.', os.path.join(home, '..', 'built', 'libSfml.so')]:
-	try: sfml=ctypes.CDLL(os.path.join(path, 'libSfml.so')); break
-	except: pass
-else: raise Exception("couldn't load lib")
+attempted_paths=[]
+def load_lib():
+	for base in ['.', os.path.join(home, '..', 'built')]:
+		for path in glob.glob(os.path.join(base, '*DansSfmlWrapper.*')):
+			try: return ctypes.CDLL(path)
+			except: attempted_paths.append(path)
+	else: raise Exception("couldn't load lib, attempted paths:\n{}".format(pprint.pformat(attempted_paths)))
+sfml=load_lib()
 assert sfml.init(640, 480, "Dan's MIDI Editor")==0
 sfml.poll_event.restype=ctypes.c_char_p
 
